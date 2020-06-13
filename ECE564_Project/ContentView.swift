ea485467a4e9d46c73c0813bcf84ac9a50a240dc
Achintya Kumar <ask60@duke.edu>
2020-06-13 18:12:10 -0400

    Initial Commit

---
+++ ECE564_Project/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  ECE564_Project
+//
+//  Created by Achintya Kumar on 6/13/20.
+//  Copyright © 2020 Achintya Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
