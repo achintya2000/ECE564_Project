dd0aa746261e766cf1d07228a68b4a4684bdddbf
Achintya Kumar <ask60@duke.edu>
2020-06-15 17:43:30 -0400

    big progress - figured out how to bind with the new helper vector class i made, can do add and sub now

--- ECE564_Project/VectorMath.swift
+++ ECE564_Project/VectorMath.swift
@@ -14,25 +14,32 @@ import Surge
 struct VectorMath: View {
     typealias Scalar = Double
     
-    let values1: Vector<Scalar> = [1, 2, 3]
-    let values2: Vector<Scalar> = [4, -5, 6]
-    
-    @State var input1 = ""
+    @State var output = ""
     @State var operation : BasicOperation = .Add
-        
+    
+    @State var v1_1 = 0.0
+    @State var v1_2 = 0.0
+    @State var v1_3 = 0.0
+    @State var v1_4 = 0.0
+    
+    @State var v2_1 = 0.0
+    @State var v2_2 = 0.0
+    @State var v2_3 = 0.0
+    @State var v2_4 = 0.0
+            
     var body: some View {
         Form {
             Section(header: Text("Vector 1")) {
                 HStack {
                     Spacer()
-                    VectorInput()
+                    VectorInput(x: $v1_1, y: $v1_2, z: $v1_3, w: $v1_4)
                     Spacer()
                 }
             }
             Section(header: Text("Vector 2")) {
                 HStack {
                     Spacer()
-                    VectorInput()
+                    VectorInput(x: $v2_1, y: $v2_2, z: $v2_3, w: $v2_4)
                     Spacer()
                 }
             }
@@ -46,21 +53,38 @@ struct VectorMath: View {
             }
             HStack {
                 Spacer()
-                Button(action: {}) {
+                Button(action: {
+                    let values1: Vector<Scalar> = [self.v1_1, self.v1_2, self.v1_3, self.v1_4]
+                    let values2: Vector<Scalar> = [self.v2_1, self.v2_2, self.v2_3, self.v2_4]
+                    
+                    switch self.operation {
+                    case .Add:
+                        let result = Surge.add(values1, values2)
+                        self.output = "\(result)"
+                        break
+                    case .Sub:
+                        let result = Surge.sub(values1, values2)
+                        self.output = "\(result)"
+                        break
+                    case .Mult:
+                        break
+                    case .Div:
+                        break
+                    }
+                }) {
                     Text("Submit")
                 }
                 Spacer()
             }
-        }.onAppear {
-            self.doWork()
+            Text(output)
         }
     }
     
     func doWork() {
-        let c = Surge.add(values1, values2)
-        print(type(of: c))
-        print(c)
-        print(c[0])
+//        let c = Surge.add(values1, values2)
+//        print(type(of: c))
+//        print(c)
+//        print(c[0])
     }
 }
 
@@ -71,26 +95,28 @@ struct VectorMath_Previews: PreviewProvider {
 }
 
 struct VectorInput: View {
-    @State var x = ""
-    @State var y = ""
-    @State var z = ""
-    @State var w = ""
+    @Binding var x: Double
+    @Binding var y: Double
+    @Binding var z: Double
+    @Binding var w: Double
+    
+    @State var dimension = 3
     
     var body: some View {
         HStack {
-            TextField("0", text: $x)
+            TextField("0", value: $x, formatter: NumberFormatter())
                 .multilineTextAlignment(.center)
                 .frame(width: 25)
                 .keyboardType(.numberPad)
-            TextField("0", text: $y)
+            TextField("0", value: $y, formatter: NumberFormatter())
                 .multilineTextAlignment(.center)
                 .frame(width: 25)
                 .keyboardType(.numberPad)
-            TextField("0", text: $z)
+            TextField("0", value: $z, formatter: NumberFormatter())
                 .multilineTextAlignment(.center)
                 .frame(width: 25)
                 .keyboardType(.numberPad)
-            TextField("0", text: $w)
+            TextField("0", value: $w, formatter: NumberFormatter())
                 .multilineTextAlignment(.center)
                 .frame(width: 25)
                 .keyboardType(.numberPad)
