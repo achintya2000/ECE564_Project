9ff2ecc1dac064d07be093206b85667274438a0a
Achintya Kumar <ask60@duke.edu>
2020-06-15 22:42:26 -0400

    improved vector math, options for 3,4,5

---
+++ ECE564_Project/Models/VectorModels.swift
@@ -0,0 +1,93 @@
+//
+//  VectorModels.swift
+//  ECE564_Project
+//
+//  Created by Achintya Kumar on 6/15/20.
+//  Copyright © 2020 Achintya Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct VectorInput3: View {
+    @Binding var x: Double
+    @Binding var y: Double
+    @Binding var z: Double
+        
+    var body: some View {
+        HStack {
+            TextField("0", value: $x, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+                .frame(width: 25)
+                .keyboardType(.numberPad)
+            TextField("0", value: $y, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+                .frame(width: 25)
+                .keyboardType(.numberPad)
+            TextField("0", value: $z, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+                .frame(width: 25)
+                .keyboardType(.numberPad)
+        }
+    }
+}
+
+struct VectorInput4: View {
+    @Binding var x: Double
+    @Binding var y: Double
+    @Binding var z: Double
+    @Binding var w: Double
+        
+    var body: some View {
+        HStack {
+            TextField("0", value: $x, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+                .frame(width: 25)
+                .keyboardType(.numberPad)
+            TextField("0", value: $y, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+                .frame(width: 25)
+                .keyboardType(.numberPad)
+            TextField("0", value: $z, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+                .frame(width: 25)
+                .keyboardType(.numberPad)
+            TextField("0", value: $w, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+                .frame(width: 25)
+                .keyboardType(.numberPad)
+        }
+    }
+}
+
+struct VectorInput5: View {
+    @Binding var x: Double
+    @Binding var y: Double
+    @Binding var z: Double
+    @Binding var w: Double
+    @Binding var v: Double
+        
+    var body: some View {
+        HStack {
+            TextField("0", value: $x, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+                .frame(width: 25)
+                .keyboardType(.numberPad)
+            TextField("0", value: $y, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+                .frame(width: 25)
+                .keyboardType(.numberPad)
+            TextField("0", value: $z, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+                .frame(width: 25)
+                .keyboardType(.numberPad)
+            TextField("0", value: $w, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+                .frame(width: 25)
+                .keyboardType(.numberPad)
+            TextField("0", value: $v, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+                .frame(width: 25)
+                .keyboardType(.numberPad)
+        }
+    }
+}
--- ECE564_Project/VectorMath.swift
+++ ECE564_Project/VectorMath.swift
@@ -14,30 +14,54 @@ import Surge
 struct VectorMath: View {
     @State var output = ""
     @State var operation : BasicOperation = .Add
+    @State var vectorSize : VectorSize = .Three
     
     @State var v1_1 = 0.0
     @State var v1_2 = 0.0
     @State var v1_3 = 0.0
     @State var v1_4 = 0.0
+    @State var v1_5 = 0.0
     
     @State var v2_1 = 0.0
     @State var v2_2 = 0.0
     @State var v2_3 = 0.0
     @State var v2_4 = 0.0
+    @State var v2_5 = 0.0
             
     var body: some View {
         Form {
+            Section(header: Text("Select Vector Size")) {
+                Picker(selection: $vectorSize, label: Text("Vector Size")) {
+                    ForEach(VectorSize.allCases, id: \.self) { size in
+                        Text(size.rawValue)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
             Section(header: Text("Vector 1")) {
                 HStack {
                     Spacer()
-                    VectorInput4(x: $v1_1, y: $v1_2, z: $v1_3, w: $v1_4)
+                    if (vectorSize == VectorSize.Three) {
+                        VectorInput3(x: $v1_1, y: $v1_2, z: $v1_3)
+                    } else if (vectorSize == VectorSize.Four) {
+                        VectorInput4(x: $v1_1, y: $v1_2, z: $v1_3, w: $v1_4)
+                    } else if (vectorSize == VectorSize.Five) {
+                        VectorInput5(x: $v1_1, y: $v1_2, z: $v1_3, w: $v1_4, v: $v1_5)
+                    }
                     Spacer()
                 }
             }
             Section(header: Text("Vector 2")) {
                 HStack {
                     Spacer()
-                    VectorInput4(x: $v2_1, y: $v2_2, z: $v2_3, w: $v2_4)
+                    if (vectorSize == VectorSize.Three) {
+                        VectorInput3(x: $v2_1, y: $v2_2, z: $v2_3)
+                    } else if (vectorSize == VectorSize.Four) {
+                        VectorInput4(x: $v2_1, y: $v2_2, z: $v2_3, w: $v2_4)
+                    } else if (vectorSize == VectorSize.Five) {
+                        VectorInput5(x: $v2_1, y: $v2_2, z: $v2_3, w: $v2_4, v: $v2_5)
+                    }
                     Spacer()
                 }
             }
@@ -52,8 +76,23 @@ struct VectorMath: View {
             HStack {
                 Spacer()
                 Button(action: {
-                    let values1: Vector<Double> = [self.v1_1, self.v1_2, self.v1_3, self.v1_4]
-                    let values2: Vector<Double> = [self.v2_1, self.v2_2, self.v2_3, self.v2_4]
+                    var values1: Vector<Double> = []
+                    var values2: Vector<Double> = []
+                    
+                    switch self.vectorSize {
+                    case .Three:
+                        values1 = [self.v1_1, self.v1_2, self.v1_3]
+                        values2 = [self.v2_1, self.v2_2, self.v2_3]
+                        break
+                    case .Four:
+                        values1 = [self.v1_1, self.v1_2, self.v1_3, self.v1_4]
+                        values2 = [self.v2_1, self.v2_2, self.v2_3, self.v2_4]
+                        break
+                    case .Five:
+                        values1 = [self.v1_1, self.v1_2, self.v1_3, self.v1_4, self.v1_5]
+                        values2 = [self.v2_1, self.v2_2, self.v2_3, self.v2_4, self.v2_5]
+                        break
+                    }
                     
                     switch self.operation {
                     case .Add:
@@ -81,6 +120,7 @@ struct VectorMath: View {
             Text("< \(output) >")
                 .font(.title)
                 .frame(maxWidth: .infinity, alignment: .center)
+            .navigationBarTitle(Text("Simple Vector Math"))
         }
     }
 }
@@ -91,63 +131,16 @@ struct VectorMath_Previews: PreviewProvider {
     }
 }
 
-struct VectorInput3: View {
-    @Binding var x: Double
-    @Binding var y: Double
-    @Binding var z: Double
-        
-    var body: some View {
-        HStack {
-            TextField("0", value: $x, formatter: NumberFormatter())
-                .multilineTextAlignment(.center)
-                .frame(width: 25)
-                .keyboardType(.numberPad)
-            TextField("0", value: $y, formatter: NumberFormatter())
-                .multilineTextAlignment(.center)
-                .frame(width: 25)
-                .keyboardType(.numberPad)
-            TextField("0", value: $z, formatter: NumberFormatter())
-                .multilineTextAlignment(.center)
-                .frame(width: 25)
-                .keyboardType(.numberPad)
-        }
-    }
-}
-
-struct VectorInput4: View {
-    @Binding var x: Double
-    @Binding var y: Double
-    @Binding var z: Double
-    @Binding var w: Double
-    
-    @State var dimension = 3
-    
-    var body: some View {
-        HStack {
-            TextField("0", value: $x, formatter: NumberFormatter())
-                .multilineTextAlignment(.center)
-                .frame(width: 25)
-                .keyboardType(.numberPad)
-            TextField("0", value: $y, formatter: NumberFormatter())
-                .multilineTextAlignment(.center)
-                .frame(width: 25)
-                .keyboardType(.numberPad)
-            TextField("0", value: $z, formatter: NumberFormatter())
-                .multilineTextAlignment(.center)
-                .frame(width: 25)
-                .keyboardType(.numberPad)
-            TextField("0", value: $w, formatter: NumberFormatter())
-                .multilineTextAlignment(.center)
-                .frame(width: 25)
-                .keyboardType(.numberPad)
-        }
-    }
-}
-
 enum BasicOperation : String, CaseIterable {
     case Add = "Add"
     case Sub = "Sub"
     case Mult = "Mult"
     case Div = "Div"
+}
+
+enum VectorSize : String, CaseIterable {
+    case Three = "3"
+    case Four = "4"
+    case Five = "5"
 }
 
