b19b81317e032bc31f60e9bf5189e4264f6bb83f
Sebastian Williams <svw9@duke.edu>
2020-06-17 22:44:35 -0400

    Got Matrix addition done

---
+++ ECE564_Project/MatrixAddView.swift
@@ -0,0 +1,21 @@
+//
+//  MatrixAddView.swift
+//  ECE564_Project
+//
+//  Created by Sebastian Williams on 6/17/20.
+//  Copyright © 2020 Achintya Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct MatrixAddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MatrixAddView_Previews: PreviewProvider {
+    static var previews: some View {
+        MatrixAddView()
+    }
+}
