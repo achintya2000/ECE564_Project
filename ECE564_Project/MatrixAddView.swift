1c698af25152227bb374bb8c9f2e9d2ed414e64b
Sebastian Williams <svw9@duke.edu>
2020-06-17 22:45:11 -0400

    More matrix add

--- ECE564_Project/MatrixAddView.swift
+++ ECE564_Project/MatrixAddView.swift
@@ -6,11 +6,71 @@
 //  Copyright © 2020 Achintya Kumar. All rights reserved.
 //
 
-import SwiftUI
+//
+//  StringMatrixView.swift
+//  ECE564_Project
+//
+//  Created by Sebastian Williams on 6/16/20.
+//  Copyright © 2020 Achintya Kumar. All rights reserved.
+//
 
+import SwiftUI
+import Surge
 struct MatrixAddView: View {
+    @State var operation: MatrixOperation = .Determinent
+    @State var result: String = ""
+    @State var matrixInput1: String = ""
+    @State var rows: Int = 0
+    @State var cols: Int = 0
+    @State var matrixInput2: String = ""
+    @State var rows2: Int = 0
+    @State var cols2: Int = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Matrix 1")){
+                Section(header: Text("Matrix")){
+                    TextField("Matrix", text: self.$matrixInput1)
+                }
+                Section(header: Text("Row/Cols")) {
+                    HStack {
+                        TextField("Rows", value: self.$rows, formatter: NumberFormatter())
+                        TextField("Cols", value: self.$cols, formatter: NumberFormatter())
+                    }
+                
+                }
+                
+            }
+            Section(header: Text("Matrix 2")){
+                Section(header: Text("Matrix")){
+                    TextField("Matrix", text: self.$matrixInput2)
+                }
+                Section(header: Text("Row/Cols")) {
+                    HStack {
+                        TextField("Rows", value: self.$rows2, formatter: NumberFormatter())
+                        TextField("Cols", value: self.$cols2, formatter: NumberFormatter())
+                    }
+                
+                }
+                
+                }
+            
+            
+            
+            Button(action: {
+                let myMatrix1 = Parser(text: self.matrixInput1, rows: self.rows, cols: self.cols).parse()
+                let myMatrix2 = Parser(text: self.matrixInput2, rows: self.rows2, cols: self.cols2).parse()
+                self.result = Surge.add(myMatrix1, myMatrix2).description
+            })
+            {
+                Text("Add")
+            }
+            Text("\(result)")
+            
+
+            
+        }
+        
+        
     }
 }
 
