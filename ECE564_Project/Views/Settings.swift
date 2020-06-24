b2e296de54c2fcc0e2d705bfcc8b8cfe20b358b9
Achintya Kumar <ask60@duke.edu>
2020-06-23 20:28:25 -0400

    settings with dark mode

---
+++ ECE564_Project/Views/Settings.swift
@@ -0,0 +1,21 @@
+//
+//  Settings.swift
+//  ECE564_Project
+//
+//  Created by Achintya Kumar on 6/23/20.
+//  Copyright © 2020 Achintya Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
